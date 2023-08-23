40efe5356ed0baf612d2e11d2e748ded1856f1ed
abeldekat <abel@nomail.com>
2023-08-23 13:44:46 +0200

    syncing current state to github

--- init.lua
+++ init.lua
@@ -1,15 +1,2 @@
---[[
-TODO: nvim-treesitter-context, "nacro90/numb.nvim"(peek a line by number)
-TODO: treesitter as foldexp, nvim-ufo(folds), mini.operators
-
-TODO: other interesting plugins:
-    "neogit", updated
-    "mbbill/undotree", primeagen <leader>u
-    "tpope/vim-fugitive", primeagen <leader>gs vim.cmd.Git
-    "lervag/vimtex",
-    "sindrets/diffview.nvim",
-    "folke/zen-mode.nvim",
---]]
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
