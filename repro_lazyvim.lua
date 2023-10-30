e3756cfd3313b660d54f459f709e295e99a1a9d5
abeldekat <abel@nomail.com>
2023-10-30 21:14:55 +0100

    syncing current state to github

---
+++ repro_lazyvim.lua
@@ -0,0 +1,28 @@
+-- Minimal `init.lua` to reproduce an issue. Save as `repro.lua` and run with `nvim -u repro.lua`
+
+-- sets std paths to use .repro and bootstraps lazy
+local function bootstrap(root) -- DO NOT change the paths
+  for _, name in ipairs({ "config", "data", "state", "cache" }) do
+    vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+  end
+  local lazypath = root .. "/plugins/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+  end
+  vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+end
+local root = vim.fn.fnamemodify("./.repro", ":p")
+bootstrap(root)
+
+-- install plugins
+local plugins = {
+  -- optional: reduce the number of plugins needed to reproduce the problem
+  { "abeldekat/lazyflex.nvim", version = "*", import = "lazyflex.hook", opts = {} },
+
+  { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+  -- add any other plugins here
+}
+require("lazy").setup(plugins, {
+  root = root .. "/plugins",
+})
+-- add anything else here
